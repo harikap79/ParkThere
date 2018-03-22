5c8b425e6a056335186048d1fcfaf93d161ec397
woodblock123456 <siyuanivan@gmail.com>
2018-03-23 01:24:12 +0800

    bookmarks
    bookmarks is ugly (as are the rest of my pages) but it works!! Things to do: compare page doesnt actually compare... it just lists one. Need to create separate comparison page where two carparks can be selected then info displayed side by side.

--- bookmark/views.py
+++ bookmark/views.py
@@ -2,6 +2,12 @@ from django.contrib.auth import login, authenticate
 from home.forms import RegistrationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from home.models import BookmarkedCarPark
 
 def BookmarkedCarParkController(request):
-    return render(request, 'bookmark/BookmarkedCarParkUI.html')
+    bmcarpark=BookmarkedCarPark.objects.filter(user = request.user)
+    if request.method == 'POST':
+        carparkID=request.POST['chosen']
+        print('debug removing bookmarked carpark '+carparkID)
+        BookmarkedCarPark.objects.filter(user = request.user,carPark__id=carparkID).delete()
+    return render(request, 'bookmark/BookmarkedCarParkUI.html',{'bmcarpark':bmcarpark})
--- carparkinfo/views.py
+++ carparkinfo/views.py
@@ -2,18 +2,33 @@ from django.contrib.auth import login, authenticate
 from home.forms import RegistrationForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from home.models import CarPark, SearchHistory
+from home.models import CarPark, SearchHistory,BookmarkedCarPark
 
 
 def CarParkInfoController(request, id):
         carpark = get_object_or_404(CarPark, id=id)
+        anon=1
         if request.user.is_anonymous:
                 print('debug anonymous user')
+                anon=1
         else:
                 print('debug info user' + request.user.email)
                 saveHis = SearchHistory()
                 saveHis.user=request.user
                 saveHis.carPark=carpark
                 saveHis.save()
-        return render(request, 'carparkinfo/CarParkInfoUI.html', {'carpark' : carpark})
+                anon=0 #not anon
+
+        if request.method == 'POST':
+                print('debug bookmarking carpark '+str(carpark.id))
+                if BookmarkedCarPark.objects.filter(user=request.user , carPark=carpark).exists():
+                        print('debug already exists')
+                else:
+                        bkm=BookmarkedCarPark()
+                        bkm.user=request.user
+                        bkm.carPark=carpark
+                        bkm.save()
+                        print('debug done bookmarking')
+                
+        return render(request, 'carparkinfo/CarParkInfoUI.html', {'carpark' : carpark , 'anon':anon})
 # Create your views here.
